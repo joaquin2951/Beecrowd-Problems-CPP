5c768232c066d9710eda3ac8b22c4d3e4d70be23
joaquin2951 <joaquin.hernandezutn@gmail.com>
2024-10-15 17:00:42 -0300

    15 OCT 2024

--- 1383-Sudoku.cpp
+++ 1383-Sudoku.cpp
@@ -11,7 +11,8 @@ bool estaNumero(const int M[], const int valor, const int TL);
 bool enterosEnCadaMatriz(const int M[][TAM]);
 bool estaNumeroEnMatriz(const int M[][TAM], const int valor, const int fil);
 bool esSudoku(const int M[][TAM], const int fc);
-
+bool estaNumeroCol(const int M[][TAM], const int valor, const  int col, const int filMAX);
+	
 int main(int argc, char *argv[]) {
 	
 	
@@ -26,15 +27,15 @@ int main(int argc, char *argv[]) {
 		
 		int M[TAM][TAM];
 		cargarMatriz(M);
-	
+		
 		cout << "Instancia " << i+1 << endl;
-	
+		
 		
 		
 		if(esSudoku(M,TAM))
-			cout << "SIM" << endl;
+			cout << "SIM" << endl << endl;
 		else
-			cout << "NAO" << endl;
+			cout << "NAO" << endl << endl;
 		
 		
 		i++;
@@ -48,7 +49,7 @@ int main(int argc, char *argv[]) {
 
 bool esSudoku(const int M[][TAM], const int fc){
 	
-		
+	
 	if(enterosEnCadaFila(M,fc)  and
 		 enterosEnCadaMatriz(M) and
 		 enterosEnCadaColumna(M,fc))
@@ -56,117 +57,112 @@ bool esSudoku(const int M[][TAM], const int fc){
 	
 	return false;
 }
-
-bool estaNumero(const int M[], const int valor, const int TL){
-	int i = 0;
-	while(i < TL){
-		if(M[i] == valor)
-			return true;
-		i++;
-	}
-	return false;
-}
-
-	bool estaNumeroEnMatriz(const int M[][TAM], const int valor, const int fil, const int col){
+	
+	
 		
-		int filaMAX = fil + 3;
-		int colACT = col - 2;
-		
-		for(int f = fil; f < filaMAX; f++){
-			for(int c = colACT ; c <= col; c++){
-				if(M[f][c] == valor)
+		bool estaNumeroEnMatriz(const int M[][TAM], const int valor, const int fil, const int col){
+			
+			int filaMAX = fil + 3;
+			int colACT = col - 2;
+			
+			for(int f = fil; f < filaMAX; f++){
+				for(int c = colACT ; c <= col; c++){
+					if(M[f][c] == valor)
+						return true;
+				}
+			}
+			
+			
+			return false;
+		}
+			
+			
+			bool enterosEnCadaMatriz(const int M[][TAM]){
+				
+				
+				for(int col = 0; col < TAM; col++){
+					
+					
+					if(col % 3 == 2){
+						
+						int numBuscado = 1;
+						
+						while(numBuscado < 10){
+							
+							if(!estaNumeroEnMatriz(M,numBuscado,0,col) or
+								 !estaNumeroEnMatriz(M,numBuscado,3,col) or
+								 !estaNumeroEnMatriz(M,numBuscado,6,col))
+								return false;
+							
+							numBuscado++;
+							
+						}
+						
+						
+					}
+					
+					
+				}
+				
+				
+				return true;
+			}
+				
+				bool estaNumeroCol(const int M[][TAM], const int valor, const  int col, const int filMAX){
+					
+					for(int f = 0; f < filMAX; f++)
+						if(M[f][col] == valor)
+							return true;
+					
+					return false;
+				}
+				
+				bool enterosEnCadaColumna(const int M[][TAM], const int colMAX){
+					
+					
+					for(int col = 0; col < colMAX; col++){
+						
+						int numero = 1;
+						while(numero < 10){
+							if(!estaNumeroCol(M,numero,col,colMAX))
+								return false;
+							numero++;
+						}
+						
+					}
+					
 					return true;
-			}
-		}
-		
-		
-		return false;
-	}
-	
-	
-	bool enterosEnCadaMatriz(const int M[][TAM]){
-		
-		
-		for(int col = 0; col < TAM; col++){
-			
-			
-			if(col % 3 == 2){
-				
-				int numBuscado = 1;
-				
-				while(numBuscado < 10){
+				}
 					
-					if(!estaNumeroEnMatriz(M,numBuscado,0,col) or
-						 !estaNumeroEnMatriz(M,numBuscado,3,col) or
-						 !estaNumeroEnMatriz(M,numBuscado,6,col))
+					
+					bool estaNumero(const int M[], const int valor, const int TL){
+						int i = 0;
+						while(i < TL){
+							if(M[i] == valor)
+								return true;
+							i++;
+						}
 						return false;
+					}
 					
-					numBuscado++;
-					
-				}
-				
-				
-			}
-			
-			
-		}
-		
-		
-		return true;
-	}
-	
-	bool enterosEnCadaColumna(const int M[][TAM], const int colMAX){
-		
-		int f = 0;
-		bool esta = true;
-		
-		while(f < TAM and esta){
-			
-			
-			int num = 1;
-			
-			
-			while(num < 10 and esta){
-				
-				esta = false;
-				int c = 0;
-				
-				while(c < TAM and !esta){//recorro columnas
-					if(M[f][c] == num)
-						esta = true;
-					
-					c++;
-				}
-				
-				
-				num++;
-			}
-			
-			f++;
-		}
-		
-		
-		return esta;
-	}
-		
-		
-		bool enterosEnCadaFila(const int M[][TAM], const int filaMAX){
-			
-			for(int fil = 0; fil < filaMAX; fil++){
-				int i = 1;
-				while(i < 10){
-					if(!estaNumero(&M[fil][0],i,filaMAX))
-						return false;
-					i++;
-				}
-			}
-			
-			return true;
-		}
-
-
-void cargarMatriz(int M[][TAM]){
-	for(int i = 0; i < TAM; i++)
-		for(int j = 0; j < TAM; j++)
-		cin >> M[i][j];
-}
+					bool enterosEnCadaFila(const int M[][TAM], const int filaMAX){
+						
+						for(int fil = 0; fil < filaMAX; fil++){
+							int i = 1;
+							while(i < 10){
+								if(!estaNumero(&M[fil][0],i,filaMAX))
+									return false;
+								i++;
+							}
+						}
+						
+						return true;
+					}
+						
+						
+						void cargarMatriz(int M[][TAM]){
+							for(int i = 0; i < TAM; i++)
+								for(int j = 0; j < TAM; j++)
+								cin >> M[i][j];
+						}
+							
